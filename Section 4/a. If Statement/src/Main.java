210557ebb4e616a558e3a7b805aea7caf9376a7c
Michael <mpeder75@gmail.com>
2023-06-16 15:00:41 +0200

    section 4 - Updated a. if statements

--- Section 4/a. If Statement/src/Main.java
+++ Section 4/a. If Statement/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Boolean variabel isAlien declares som assignes til at være false
+        boolean isAlien = false;
+
+        // If statement tjekker sin condition om variablen isAlive er falsk - evalueres det til true, køres code block
+        if (isAlien == false)
+        {
+            System.out.println("It is not a Alien");
+            System.out.println("Ani I an scared of aliens");
+        }
+
     }
 }
--- Section 4/src/Hello.java
+++ Section 4/src/Hello.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Hello {
+public class Hello {
+
+    public static void main(String[] args) {
+
+        // For at printe til en anden linje brug \n
+        System.out.printf("Hello, Tim\n");
+        System.out.println("Hello World");
+
+    }
+
 }
