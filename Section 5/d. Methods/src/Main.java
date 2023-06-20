f84049d41e597cb3caae8c718ac1d8446f2c14df
Michael <mpeder75@gmail.com>
2023-06-20 16:02:47 +0200

    section 5 - Created d. Methods

---
+++ Section 5/d. Methods/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 1000;
+        int levelCompleted = 8;
+        int bonus = 200;
+
+       int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+}
+
+
