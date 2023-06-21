377596b187a827235f6712955411379d386052f0
Michael <mpeder75@gmail.com>
2023-06-21 08:50:05 +0200

    section 5 - Created g. Coding Exercise 1

--- Section 5/e. Methods 2/method return/src/Main.java
+++ Section 5/e. Methods 2/method return/src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        // calculateScore kaldes med argumenter, og resultat calculateScore returnere gemmes i variablen highScore
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("The highScore is " + highScore);
+
+        // nu assignes variable med ny værdier
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 8;
+
+        // Her printer man DIREKTE resultatet fra calculateScore
+        System.out.println("The new highScore is " + calculateScore(gameOver,score,levelCompleted,bonus));
+
+    }
+
+
+    // method calculateScore med 4 parameter der returnere en int "finalScore"
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+        }
+
+        return finalScore;
     }
 }
+
+
+
+
+
--- Section 5/e. Methods 2/src/Main.java
+++ Section 5/e. Methods 2/src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        // 1. Her kaldes metoden calculateScore med 4 argumenter i form af variabel navne ovenfor
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        // 2. Nu assignes variablerne nye værdier
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        // 3. Nu kaldes calculateScore med de nye værdier, der er assignet ovenfor
+        calculateScore(gameOver,score,levelCompleted,bonus);
+
+    }
+
+
+    // method calculateScore med 4 parameter
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("your finalScore was " + finalScore);
+        }
+    }
+
+
+    // method der returnere beregning af to tal
+
+    public static int calculateNumbers(int a, int b)
+    {
+        int calculatedNumber = a + b;
+        return calculatedNumber;
     }
 }
+
+
